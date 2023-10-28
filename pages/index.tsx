a772575a7fde392d2c0c5ca8909cd025fe3ba89c
ZenX777 <139478072+ZenX777@users.noreply.github.com>
2023-10-29 02:18:13 +0530

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import { NextPage } from "next";
+import NextLink from 'next/link'
+import { Button, Container, Flex, Heading, Stack } from "@chakra-ui/react";
+
+const Home: NextPage = () => {
+  return (
+    <Container maxW={"1200px"}>
+    <Flex h={"80vh"} alignItems={"center"} justifyContent={"center"}>
+      <Stack spacing={4} align={"center"}>
+        <Heading>Marketplace</Heading>
+        <Button
+           as={NextLink} href='/buy'
+        >Shop NFTs</Button>
+      </Stack>
+    </Flex>
+  </Container>
+      
+  );
+};
+
+export default Home;
